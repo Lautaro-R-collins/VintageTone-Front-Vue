c6cd8f92c2ecd8af31ecca46507b8483539882c3
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-02 20:08:50 -0300

    add toast

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -2,10 +2,12 @@
 import Navbar from '../components/navbar/Navbar.vue'
 import Footer from '../components/footer/Footer.vue'
 import AnnouncementBar from '../components/common/AnnouncementBar.vue'
+import { Toaster } from 'vue-sonner'
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col bg-slate-50 selection:bg-amber-100 selection:text-amber-900">
+    <Toaster position="top-right" richColors />
     <AnnouncementBar />
     <Navbar />
     
