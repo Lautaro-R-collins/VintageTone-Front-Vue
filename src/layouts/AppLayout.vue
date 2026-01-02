1ab00922e61977edcfb523ce01c500e90e4216b5
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-02 17:24:59 -0300

    Refactor Layout

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Navbar from '../components/navbar/Navbar.vue'
+import Footer from '../components/footer/Footer.vue'
+import AnnouncementBar from '../components/common/AnnouncementBar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col bg-slate-50 selection:bg-amber-100 selection:text-amber-900">
+    <AnnouncementBar />
+    <Navbar />
+    
+    <main class="grow">
+      <slot />
+    </main>
+    
+    <Footer />
+  </div>
+</template>
