77e0f7767c8ac34a0254341c3caffa3f20eef486
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-03 20:57:37 -0300

    add components in home ( HomeOgert and InfoCardHome components)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@ import { ref, computed } from 'vue'
 import HeaderHome from '../components/home/HeaderHome.vue'
 import SliderBanner from '../components/home/SliderBanner.vue'
 import CategoryHome from '../components/home/CategoryHome.vue'
+import InfoCardsHome from '../components/home/InfoCardsHome.vue'
+import HomeOfert from '../components/home/HomeOfert.vue'
 import CardProduct from '../components/product/CardProduct.vue'
 import BackBanner from '../components/home/BackBanner.vue'
 import Pagination from '../components/ui/Pagination.vue'
@@ -26,6 +28,8 @@ const paginatedProducts = computed(() => {
     <HeaderHome />
     <SliderBanner />
     <CategoryHome />
+    <InfoCardsHome />
+    <HomeOfert />
 
     <div class="max-w-7xl mx-auto px-6 pb-20">
       <h2 class="text-3xl font-black text-slate-900 uppercase italic tracking-tighter mb-12">
