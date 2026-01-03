bfddd63792cb30476fbd78d265a93d8e17fb898e
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-03 03:31:52 -0300

    Refactor component - home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,17 +33,10 @@ const paginatedProducts = computed(() => {
       </h2>
 
       <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
-        <CardProduct 
-          v-for="product in paginatedProducts" 
-          :key="product.id" 
-          :product="product"
-        />
+        <CardProduct v-for="product in paginatedProducts" :key="product.id" :product="product" />
       </section>
 
-      <Pagination 
-        v-model:currentPage="currentPage" 
-        :totalPages="totalPages" 
-      />
+      <Pagination v-model:currentPage="currentPage" :totalPages="totalPages" />
     </div>
 
     <BackBanner />
