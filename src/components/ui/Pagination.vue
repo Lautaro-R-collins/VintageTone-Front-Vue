24bc74761eca41c5caed4b4db738df6215ce8662
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-03 03:32:33 -0300

    Refactor component - pagination

--- src/components/ui/Pagination.vue
+++ src/components/ui/Pagination.vue
@@ -22,36 +22,27 @@ const setPage = (page) => {
 <template>
     <div class="flex justify-center mt-12 mb-20">
         <div class="flex items-center gap-2">
-            <button 
+            <button
                 class="btn btn-sm md:btn-md bg-slate-900 hover:bg-slate-950 text-white border-none rounded-lg transition-all duration-300 disabled:opacity-30 cursor-pointer"
-                :disabled="currentPage === 1"
-                @click="emit('update:currentPage', currentPage - 1)"
-            >
+                :disabled="currentPage === 1" @click="emit('update:currentPage', currentPage - 1)">
                 «
             </button>
-            
-            <button 
-                v-for="page in totalPages" 
-                :key="page"
+
+            <button v-for="page in totalPages" :key="page"
                 class="btn btn-sm md:btn-md border-none rounded-lg transition-all duration-300 font-bold cursor-pointer"
                 :class="[
-                    currentPage === page 
-                    ? 'bg-slate-950 text-white shadow-lg' 
-                    : 'bg-slate-900 hover:bg-slate-950 text-white'
-                ]"
-                @click="emit('update:currentPage', page)"
-            >
+                    currentPage === page
+                        ? 'bg-slate-950 text-white shadow-lg'
+                        : 'bg-slate-900 hover:bg-slate-950 text-white'
+                ]" @click="emit('update:currentPage', page)">
                 {{ page }}
             </button>
 
-            <button 
+            <button
                 class="btn btn-sm md:btn-md bg-slate-900 hover:bg-slate-950 text-white border-none rounded-lg transition-all duration-300 disabled:opacity-30 cursor-pointer"
-                :disabled="currentPage === totalPages"
-                @click="emit('update:currentPage', currentPage + 1)"
-            >
+                :disabled="currentPage === totalPages" @click="emit('update:currentPage', currentPage + 1)">
                 »
             </button>
         </div>
     </div>
 </template>
-
