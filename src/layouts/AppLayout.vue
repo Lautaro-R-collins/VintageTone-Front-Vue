665f2f9106195c1237471353bdcea6f94a7ea107
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-03 22:14:07 -0300

    refactor layout (coment AnnouncementBar - component)

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,14 +1,14 @@
 <script setup>
 import Navbar from '../components/navbar/Navbar.vue'
 import Footer from '../components/footer/Footer.vue'
-import AnnouncementBar from '../components/common/AnnouncementBar.vue'
+// import AnnouncementBar from '../components/common/AnnouncementBar.vue'
 import { Toaster } from 'vue-sonner'
 </script>
 
 <template>
   <div class="min-h-screen flex flex-col bg-slate-50 selection:bg-amber-100 selection:text-amber-900">
     <Toaster position="top-right" richColors />
-    <AnnouncementBar />
+    <!-- <AnnouncementBar /> -->
     <Navbar />
 
     <main class="grow">
