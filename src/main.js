c7f90647e995a318cfb5419f3cd50eca099808a5
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-04 07:07:54 -0300

    chore: setup pinia in main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import './style.css'
+// Toast styles
+import 'vue-sonner/style.css'
 import App from './App.vue'
 
 const app = createApp(App)
