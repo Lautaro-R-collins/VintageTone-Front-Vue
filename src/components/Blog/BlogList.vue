cce47fc5d03eb41f6fbc7bd79d99c3d002f2e781
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-03 22:17:14 -0300

    add nwe component BlogList

---
+++ src/components/Blog/BlogList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { mockBlogPosts } from '../../mocks/mockBlog'
+import BlogCard from './BlogCard.vue'
+</script>
+
+<template>
+    <div class="max-w-7xl mx-auto px-6 py-20">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <BlogCard 
+                v-for="post in mockBlogPosts" 
+                :key="post.id" 
+                :post="post" 
+            />
+        </div>
+    </div>
+</template>
