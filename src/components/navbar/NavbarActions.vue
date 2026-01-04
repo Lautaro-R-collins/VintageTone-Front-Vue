fef79386951943742bd7cf7ca80e284e3d3f7ab5
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-04 07:09:23 -0300

    style: update Navbar layout and components

--- src/components/navbar/NavbarActions.vue
+++ src/components/navbar/NavbarActions.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { ShoppingBag } from 'lucide-vue-next'
+import { useCartStore } from '../../stores/cart'
 import AuthButton from './AuthButton.vue'
+
+const cartStore = useCartStore()
 </script>
 
 <template>
     <div class="flex items-center space-x-4 sm:space-x-6">
+        <!-- Cart Icon -->
+        <button
+            @click="cartStore.toggleDrawer()"
+            class="cursor-pointer relative group"
+        >
+            <ShoppingBag :size="25" class="group-hover:scale-110 transition-transform" />
+            <!-- Badge -->
+            <span v-if="cartStore.totalItems > 0"
+                class="absolute -top-1.5 -right-1.5 bg-amber-500 text-slate-950 font-black text-[9px] w-5 h-5 flex items-center justify-center rounded-full border-2 border-white">
+                {{ cartStore.totalItems }}
+            </span>
+        </button>
+
         <AuthButton />
     </div>
 </template>
