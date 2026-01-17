ecd4d9216343bb741400b4410c5dafe131010bcd
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-17 12:13:36 -0300

    fix ulr

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+export const BASE_URL = (import.meta.env.VITE_API_URL || 'http://localhost:3000').replace(/\/$/, '')
 const API_URL = BASE_URL
 
 const api = axios.create({
