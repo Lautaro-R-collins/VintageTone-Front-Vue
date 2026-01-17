53eb8ca7c4573ce489677a5ebd86ad3809c0f1c8
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-17 17:19:05 -0300

    Add productSkeleton

---
+++ src/components/product/ProductSkeleton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="aspect-4/5 bg-slate-100 rounded-xl overflow-hidden relative border border-slate-200 p-8 flex flex-col justify-end gap-3">
+        <div class="skeleton h-full w-full absolute inset-0"></div>
+        <div class="relative z-10 space-y-3">
+            <div class="skeleton h-3 w-20"></div>
+            <div class="skeleton h-8 w-full"></div>
+            <div class="skeleton h-6 w-24"></div>
+            <div class="skeleton h-4 w-32"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
